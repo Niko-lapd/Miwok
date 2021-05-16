0146449cf82acfd681ca6213e847b76a8a0b2012
Nikolay <niko.lapdgot@gmail.com>
2021-05-16 12:20:24 +0200

    Base

---
+++ jdk8.bat
@@ -0,0 +1,5 @@
+@ echo off
+set JAVA_HOME="C:\Program Files\Java\jre1.8.0_291"
+SET PATH=%JAVA_HOME%/bin;%PATH%
+echo Display java Version
+java -version
---
+++ jdk9.bat
@@ -0,0 +1,5 @@
+@ echo off
+set JAVA_HOME="C:\Program Files\Java\jdk-9"
+SET PATH=%JAVA_HOME%/bin;%PATH%
+echo Display java Version
+java -version
